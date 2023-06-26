b68d5f9601fccadcaf54f1d38743210a4fd7c6de
kdecol <132307184+kdecol@users.noreply.github.com>
2023-06-27 09:39:12 +1200

    created a welcome message
    v1 of welcome message, welcomes the customer with a randomly picked name.

---
+++ Audio/components/welcome v1.py
@@ -0,0 +1,13 @@
+import random
+from random import randint
+
+#List of names
+names = ["Mark", "Allen", "kade", "Sean", "Luka", "Jayden", "Mk", "Daniel", "Joshua" "Holly"]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print("*** Welcome to KSC Audio")
+print("*** My name is", name, "***")
+print("***I will be here to help you order your perfect audio setup***")
