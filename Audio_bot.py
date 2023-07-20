3c676b0a18dd70e1240c2a6d37b01bbdedf7a454
kdecol <132307184+kdecol@users.noreply.github.com>
2023-07-20 14:31:18 +1200

    Added comments

--- Audio_bot.py
+++ Audio_bot.py
@@ -1,6 +1,33 @@
+# Audio bot program
+
+
 import random
 from random import randint
 
-#List of names
+# List of names
 names = ["Mark", "Allen", "kade", "Sean", "Luka", "Jayden", "Mk", "Daniel", "Joshua" "Holly"]
+# Customer details dictionary
 
+# Welcome message with random names
+
+# Validates inputs to cheack if they are blank
+
+# Menu for click and collect or delivery
+
+# Click and collect information - name and phone number
+
+# Delivery information - name, adress and phone
+
+# Choose total number of products 
+
+# List of audio products
+
+# Order - from store list - print each product with cost
+
+# Print order out - including if order is for delivery or click and collect and names and prices
+
+# Ability to cancel or proceed with order
+
+# Options for new order or to exit
+
+# Main function
