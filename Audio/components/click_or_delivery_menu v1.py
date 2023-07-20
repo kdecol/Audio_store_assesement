152ed330ff8018b4325b32809085fd188b887d43
kdecol <132307184+kdecol@users.noreply.github.com>
2023-07-20 14:16:50 +1200

    click and collect or delivery v1

---
+++ Audio/components/click_or_delivery_menu v1.py
@@ -0,0 +1,16 @@
+# Bugs
+# will only work for valid input "d" and "p"
+
+# menu so that user can choose either click and collect or delivery
+
+print ("Do you want your order delivered or would you like to do click and collect?")
+
+print (" For delivery enter d or enter p for click and collect")
+
+delivery = input()
+
+if delivery == "d":
+    print ("Delivery")
+
+elif delivery == "p":
+    print ("Pickup")
---
+++ Audio_bot.py
@@ -0,0 +1,6 @@
+import random
+from random import randint
+
+#List of names
+names = ["Mark", "Allen", "kade", "Sean", "Luka", "Jayden", "Mk", "Daniel", "Joshua" "Holly"]
+
