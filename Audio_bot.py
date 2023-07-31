e5c2b4e9c997e9c743f6f980cf388499220d68ed
kdecol <132307184+kdecol@users.noreply.github.com>
2023-07-31 12:08:58 +1200

    created audio order print v1.
    prints out order and prices

--- Audio_bot.py
+++ Audio_bot.py
@@ -101,17 +101,6 @@ def delivery_info():
 
 
 # Choose total number of products 
-
-
-
-# List of audio products
-audio_names = ['Sony 12inch subs','Car amp','Sony tweeters','JBL tweeters','JBL single 10inch sub','AUX cord','Pioneer head unit','Double din Sony head unit',
-               'Bluetooth audio adapter','Sub install kit','FM transmitter','Quad channel amp']
-
-audio_prices = [300, 150, 85, 60, 175, 22.50, 425, 465, 30.50, 69.99, 37.70, 230]
-
-
-
 #print("How many pizzas would you like to order? ")
 #num_pizza = int(input())
 def menu():
