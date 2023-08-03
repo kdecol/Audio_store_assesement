2350b9fbe2a54cfa0d8b4ddd300e30af49bcf242
kdecol <132307184+kdecol@users.noreply.github.com>
2023-08-03 15:06:56 +1200

    Created cancel confirm function

---
+++ Audio/components/confirm_cancel_order v1.py
@@ -0,0 +1,19 @@
+print ("Please confirm your order")
+print ("To confirm please enter 1")
+print ("To cancel please enter 2")
+while True:
+    try:
+        confirm = int(input("Please enter a number "))
+        if confirm >= 1 and confirm <= 2:
+            if confirm == 1:
+                print("Order confirmed")
+                break
+
+            elif confirm == 2:
+                print("Order cancelled")
+                break
+        else:
+            print("The number must be 1 or 2")
+    except ValueError:
+        print("That is not a valid number")
+        print("Please enter 1 or 2")
--- Audio_bot.py
+++ Audio_bot.py
@@ -174,7 +174,26 @@ def print_order(del_pick):
 
 
 # Ability to cancel or proceed with order
+def cancel_proceed():
+    print ("Please confirm your order")
+    print ("To confirm please enter 1")
+    print ("To cancel please enter 2")
+    while True:
+        try:
+            confirm = int(input("Please enter a number "))
+            if confirm >= 1 and confirm <= 2:
+                if confirm == 1:
+                    print("Order confirmed")
+                    break
 
+                elif confirm == 2:
+                    print("Order cancelled")
+                    break
+            else:
+                print("The number must be 1 or 2")
+        except ValueError:
+            print("That is not a valid number")
+            print("Please enter 1 or 2")
 
 
 # Options for new order or to exit
@@ -195,6 +214,7 @@ def main():
     menu()
     order_audio()
     print_order(del_pick)
+    cancel_proceed()
    
 
 main()
